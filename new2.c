d310b015e929fee84e3a608173d12b6517c5cb5a
thecodersharma <97011764+thecodersharma@users.noreply.github.com>
2024-11-07 12:45:27 +0530

    Add files via upload

---
+++ new2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+#include<ctype.h>
+int main(int argc, char const *argv[])
+{char ch;
+int age;
+	printf("enter age: \n",age);
+	scanf("%d",&age);
+    if(age>18){
+    	printf("adult");
+    }
+else{printf("not adult");
+
+}
+
+	
+
+	return 0;
+}    
